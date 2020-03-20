f1139e6231d8ac448310feb7ffd555cf1e7b71eb
Sang-Ki Ko <narame7@gmail.com>
2020-03-20 12:40:47 +0000

    Random Move Agent

--- agents/base.py
+++ agents/base.py
@@ -1,8 +1,26 @@
 import numpy as np
 
+
 class BaseAgent:
 
     def choose_action(self, game):
         action = np.random.choice(game.actions())
         print("Choosing action {action}".format(action=action))
         return action
+
+
+class RandomAgent:
+
+    def choose_action(self, game):
+        action = np.random.choice(game.actions())
+        print("Choosing action {action}".format(action=action))
+        return action
+
+class RandomMoveAgent:
+    def choose_action(self, game):
+        legal_actions = game.actions()
+        
+        legal_move_actions = np.intersect1d(np.arange(12), legal_actions)
+
+        return np.random.choice(legal_move_actions)
+
