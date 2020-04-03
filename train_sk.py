47c4358ea7fe6825d306607585c7298a1848814f
Clarit7 <inar2157@gmail.com>
2020-04-03 19:12:43 +0900

    Fix dimensional error in get_equi_data().

--- train_sk.py
+++ train_sk.py
@@ -56,11 +56,11 @@ class TrainPipeline(object):
 
         extend_data = []
         for i, (state, mcts_prob, winner) in enumerate(play_data):
-
-
-            ## horizontally flipped game
-
             wall_state = state[:3,:BOARD_SIZE - 1,:BOARD_SIZE - 1]
+            dist_state1 = np.reshape(state[(6 + (WALL_NUM + 1) * 2), :BOARD_SIZE, :BOARD_SIZE], (1, BOARD_SIZE, BOARD_SIZE))
+            dist_state2 = np.reshape(state[(7 + (WALL_NUM + 1) * 2), :BOARD_SIZE, :BOARD_SIZE], (1, BOARD_SIZE, BOARD_SIZE))
+
+            # horizontally flipped game
             flipped_wall_state = []
 
             for i in range(3):
@@ -69,7 +69,6 @@ class TrainPipeline(object):
                 flipped_wall_state.append(wall_padded)
 
             flipped_wall_state = np.array(flipped_wall_state)
-
 
             player_position = state[3:5, :,:]
 
@@ -81,17 +80,16 @@ class TrainPipeline(object):
 
             h_equi_state = np.vstack([flipped_wall_state, flipped_player_position, state[5:, :,:]])
 
-
             h_equi_mcts_prob = np.copy(mcts_prob)
 
-            h_equi_mcts_prob[9] = mcts_prob[8]
-            h_equi_mcts_prob[8] = mcts_prob[9]
-            h_equi_mcts_prob[10] = mcts_prob[11]
-            h_equi_mcts_prob[11] = mcts_prob[10]
-            h_equi_mcts_prob[7] = mcts_prob[6]
-            h_equi_mcts_prob[6] = mcts_prob[7]
-            h_equi_mcts_prob[2] = mcts_prob[3]
-            h_equi_mcts_prob[3] = mcts_prob[2]
+            h_equi_mcts_prob[11] = mcts_prob[10]  # SE to SW
+            h_equi_mcts_prob[10] = mcts_prob[11]  # SW to SE
+            h_equi_mcts_prob[9] = mcts_prob[8]    # NE to NW
+            h_equi_mcts_prob[8] = mcts_prob[9]    # NW to NE
+            h_equi_mcts_prob[7] = mcts_prob[6]    # EE to WW
+            h_equi_mcts_prob[6] = mcts_prob[7]    # WW to EE
+            h_equi_mcts_prob[3] = mcts_prob[2]    # E to W
+            h_equi_mcts_prob[2] = mcts_prob[3]    # W to E
 
             h_wall_actions = h_equi_mcts_prob[12:12 + (BOARD_SIZE-1) ** 2].reshape(BOARD_SIZE-1, BOARD_SIZE-1)
             v_wall_actions = h_equi_mcts_prob[12 + (BOARD_SIZE-1) ** 2:].reshape(BOARD_SIZE-1, BOARD_SIZE -1)
@@ -101,9 +99,7 @@ class TrainPipeline(object):
 
             h_equi_mcts_prob[12:] = np.hstack([flipped_h_wall_actions.flatten(), flipped_v_wall_actions.flatten()])
 
-
-            ## Vertically flipped game
-
+            # Vertically flipped game
             flipped_wall_state = []
 
             for i in range(3):
@@ -122,20 +118,20 @@ class TrainPipeline(object):
 
             cur_player = (np.ones((BOARD_SIZE, BOARD_SIZE)) - state[5 + 2* (WALL_NUM+1),:,:]).reshape(-1,BOARD_SIZE, BOARD_SIZE)
 
-            v_equi_state = np.vstack([flipped_wall_state, flipped_player_position, state[5+(WALL_NUM+1):5 + 2*(WALL_NUM+1), :,:], state[5:5+(WALL_NUM+1),:,:], cur_player])
+            v_equi_state = np.vstack([flipped_wall_state, flipped_player_position, state[5+(WALL_NUM+1):5 + 2*(WALL_NUM+1), :,:], state[5:5+(WALL_NUM+1),:,:], cur_player, dist_state2, dist_state1])
 
 
 
             v_equi_mcts_prob = np.copy(mcts_prob)
 
-            v_equi_mcts_prob[4] = mcts_prob[5]
-            v_equi_mcts_prob[9] = mcts_prob[11]
-            v_equi_mcts_prob[11] = mcts_prob[9]
-            v_equi_mcts_prob[8] = mcts_prob[10]
-            v_equi_mcts_prob[10] = mcts_prob[8]
-            v_equi_mcts_prob[5] = mcts_prob[4]
-            v_equi_mcts_prob[0] = mcts_prob[1]
-            v_equi_mcts_prob[1] = mcts_prob[0]
+            v_equi_mcts_prob[11] = mcts_prob[9]  # SE to NE
+            v_equi_mcts_prob[10] = mcts_prob[8]  # SW to NW
+            v_equi_mcts_prob[9] = mcts_prob[11]  # NE to SE
+            v_equi_mcts_prob[8] = mcts_prob[10]  # NW to SW
+            v_equi_mcts_prob[5] = mcts_prob[4]   # NN to SS
+            v_equi_mcts_prob[4] = mcts_prob[5]   # SS to NN
+            v_equi_mcts_prob[1] = mcts_prob[0]   # N to S
+            v_equi_mcts_prob[0] = mcts_prob[1]   # S to N
 
             h_wall_actions = v_equi_mcts_prob[12:12 + (BOARD_SIZE-1) ** 2].reshape(BOARD_SIZE-1, BOARD_SIZE-1)
             v_wall_actions = v_equi_mcts_prob[12 + (BOARD_SIZE-1) ** 2:].reshape(BOARD_SIZE-1, BOARD_SIZE -1)
@@ -167,24 +163,22 @@ class TrainPipeline(object):
 
             cur_player = (np.ones((BOARD_SIZE, BOARD_SIZE)) - state[5 + 2*(WALL_NUM+1),:,:]).reshape(-1,BOARD_SIZE, BOARD_SIZE)
 
-            hv_equi_state = np.vstack([flipped_wall_state, flipped_player_position, state[5 + (WALL_NUM+1):5 + 2*(WALL_NUM+1), :,:], state[5:5+(WALL_NUM+1),:,:], cur_player])
-
-
+            hv_equi_state = np.vstack([flipped_wall_state, flipped_player_position, state[5 + (WALL_NUM+1):5 + 2*(WALL_NUM+1), :,:], state[5:5+(WALL_NUM+1),:,:], cur_player, dist_state2, dist_state1])
 
             hv_equi_mcts_prob = np.copy(mcts_prob)
 
-            hv_equi_mcts_prob[4] = mcts_prob[5]
-            hv_equi_mcts_prob[9] = mcts_prob[10]
-            hv_equi_mcts_prob[10] = mcts_prob[9]
-            hv_equi_mcts_prob[8] = mcts_prob[11]
-            hv_equi_mcts_prob[11] = mcts_prob[8]
-            hv_equi_mcts_prob[5] = mcts_prob[4]
-            hv_equi_mcts_prob[0] = mcts_prob[1]
-            hv_equi_mcts_prob[1] = mcts_prob[0]
-            hv_equi_mcts_prob[2] = mcts_prob[3]
-            hv_equi_mcts_prob[3] = mcts_prob[2]
-            hv_equi_mcts_prob[6] = mcts_prob[7]
-            hv_equi_mcts_prob[7] = mcts_prob[6]
+            hv_equi_mcts_prob[11] = mcts_prob[8]  # SE to NW
+            hv_equi_mcts_prob[10] = mcts_prob[9]  # SW to NE
+            hv_equi_mcts_prob[9] = mcts_prob[10]  # NE to SW
+            hv_equi_mcts_prob[8] = mcts_prob[11]  # NW to SE
+            hv_equi_mcts_prob[7] = mcts_prob[6]   # EE to WW
+            hv_equi_mcts_prob[6] = mcts_prob[7]   # WW to EE
+            hv_equi_mcts_prob[5] = mcts_prob[4]   # NN to SS
+            hv_equi_mcts_prob[4] = mcts_prob[5]   # SS to NN
+            hv_equi_mcts_prob[3] = mcts_prob[2]   # E to W
+            hv_equi_mcts_prob[2] = mcts_prob[3]   # W to E
+            hv_equi_mcts_prob[1] = mcts_prob[0]   # N to S
+            hv_equi_mcts_prob[0] = mcts_prob[1]   # S to N
 
             h_wall_actions = hv_equi_mcts_prob[12:12 + (BOARD_SIZE-1) ** 2].reshape(BOARD_SIZE-1, BOARD_SIZE-1)
             v_wall_actions = hv_equi_mcts_prob[12 + (BOARD_SIZE-1) ** 2:].reshape(BOARD_SIZE-1, BOARD_SIZE -1)
@@ -193,7 +187,6 @@ class TrainPipeline(object):
             flipped_v_wall_actions = np.fliplr(np.flipud(v_wall_actions))
 
             hv_equi_mcts_prob[12:] = np.hstack([flipped_h_wall_actions.flatten(), flipped_v_wall_actions.flatten()])
-
 
             ###########
 
@@ -291,7 +284,7 @@ class TrainPipeline(object):
                     print("current self-play batch: {}".format(i + 1))
                     # win_ratio = self.policy_evaluate()
                     # Add generation to filename
-                    self.policy_value_net.save_model('model_' + str(count) + '_' + str("%0.3f_" % (valloss+polloss) + str(time.strftime('%Y-%m-%d', time.localtime(time.time())))))
+                    self.policy_value_net.save_model('model_a_' + str(count) + '_' + str("%0.3f_" % (valloss+polloss) + str(time.strftime('%Y-%m-%d', time.localtime(time.time())))))
         except KeyboardInterrupt:
             print('\n\rquit')
 
