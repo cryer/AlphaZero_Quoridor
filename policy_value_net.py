4ed1723370deefd1b6aff185e605dcd30a369951
Clarit7 <inar2157@gmail.com>
2020-04-21 15:55:24 +0900

    Fix dimensional error

--- policy_value_net.py
+++ policy_value_net.py
@@ -81,7 +81,7 @@ class policy_value_net(nn.Module):
         self.conv3 = nn.Conv2d(dim, 2, kernel_size=3, stride=stride,
                                padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(2)
-        self.fc3 = nn.Linear(BOARD_SIZE * 2 ** 2, (BOARD_SIZE - 1) ** 2 * 2 + 12)
+        self.fc3 = nn.Linear(BOARD_SIZE ** 2 * 2, (BOARD_SIZE - 1) ** 2 * 2 + 12)
 
 
     def forward(self,x):
